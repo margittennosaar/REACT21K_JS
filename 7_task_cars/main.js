1ffefe757e674ba840f2992e28630237cc4a0ed8
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2021-02-16 14:04:02 +0200

    dom manipulations

--- 7_task_cars/main.js
+++ 7_task_cars/main.js
@@ -7,17 +7,27 @@ function Car(licence, maker, model, price, color) {
   this.carModel = model;
   this.carPrice = price;
   this.carColor = color;
+  this.discount = function () {
+    if (this.carPrice >= 20000) {
+      this.discount = this.carPrice * 0.25;
+    } else if (this.carPrice <= 5000) {
+      this.discount = this.carPrice * 0.1;
+    } else {
+      this.discount = this.carPrice * 0.15;
+    }
+  };
+  this.discount();
 }
 
 let carArray = [];
 
 const addCar = (event) => {
   event.preventDefault();
-  let licence = document.getElementById("licence").value;
-  let maker = document.getElementById("maker").value;
-  let model = document.getElementById("model").value;
-  let price = document.getElementById("price").value;
-  let color = document.getElementById("color").value;
+  let licence = document.querySelector("#licence").value;
+  let maker = document.querySelector("#maker").value;
+  let model = document.querySelector("#model").value;
+  let price = document.querySelector("#price").value;
+  let color = document.querySelector("#color").value;
   let newCar = new Car(licence, maker, model, price, color);
   carArray.push(newCar);
   console.table(carArray);
@@ -27,21 +37,20 @@ const addCar = (event) => {
 const searchCar = (event) => {
   event.preventDefault();
 
-  let licence = document.getElementById("searchLicence").value;
+  let licence = document.querySelector("#searchLicence").value;
   let text = document.querySelector("p");
-
+  formSearch.reset();
   if (carArray.length == 0) {
     text.textContent = "Please enter car first";
   } else {
-    for (let i = 0; i < carArray.length; i++) {
-      if (carArray[i].licencePlate == licence) {
-        text.textContent = `Licence number ${carArray[i].licencePlate} is ${carArray[i].manufacturer} ${carArray[i].carModel} `;
+    for (const i of carArray) {
+      if (i.licencePlate == licence) {
+        return (text.textContent = `Licence number ${i.licencePlate} is ${i.manufacturer} ${i.carModel} and your discount is ${i.discount}`);
       } else {
-        text.textContent = "whaaaaat?";
+        text.textContent = `There is no car with that licence plate added to the system. Try again?`;
       }
     }
   }
-  formSearch.reset();
 };
 
 formInput.addEventListener("submit", addCar);
